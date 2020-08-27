a4caed5659c1e662267a9864bb4f82de87bbc736
takat0627 <takat.fb.xavi@gmail.com>
2020-08-28 03:26:00 +0900

    "[5.9] フォームから値を取得"

--- tutorial/dev44/sample4/public/javascripts/index.js
+++ tutorial/dev44/sample4/public/javascripts/index.js
@@ -1,6 +1,9 @@
+/* jQueryのセレクタを使って要素の取得や代入を行う */
+
 // 要素の値を取得する
+// テキストフォーム（id=text1）のvalueを取得する
 function getValue() {
-    const text1Value = '';
+    const text1Value = $('#text1').val();
     return text1Value;
 }
 
@@ -8,13 +11,15 @@ function getValue() {
 function popupValue() {
     const text1Value = getValue();
     // ここに条件分岐を記入する
-
+    // テキストボックスの中身が空白じゃなければ -> 空なら何も処理を実行しない
+    if (text1Value!== ""){
         alert(text1Value);
-
+    }
 }
 
 // 要素に値をセットする
+// getの逆で，値をdivタグ（id=text2）に代入する．
 function setValue() {
     const text2Value = 'Rakus!';
-
+    $('#text2').text(text2Value);
 }
