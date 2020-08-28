98e458cad724b14d9f76aefa8cce1680a449621d
takat0627 <takat.fb.xavi@gmail.com>
2020-08-28 23:57:44 +0900

    追加実装の修正（入退室の日時表示）

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -1,12 +1,12 @@
 'use strict';
 
+require('date-utils');
+
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    require('date-utils');
-    let dt = new Date();
-    let dtFormat = dt.toFormat('DDD MMM DD YYYY HH24:MI:SS');
-
     socket.on('sendEnterEvent', function (data) {
+        let dt = new Date();
+        let dtFormat = dt.toFormat('DDD MMM DD YYYY HH24:MI:SS');
         console.log('入室ユーザ名：' + data);
         socket.broadcast.emit('receiveEnterEvent', data, dtFormat);
     });
--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -3,11 +3,13 @@
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
 
-    require('date-utils');
-    let dt = new Date();
-    let dtFormat = dt.toFormat('DDD MMM DD YYYY HH24:MI:SS');
-
+    // ここは入室時と共通か？
+    // require('date-utils');
+    // let dt = new Date();
+    
     socket.on('sendExitEvent', function (data) {
+        let dt = new Date();
+        let dtFormat = dt.toFormat('DDD MMM DD YYYY HH24:MI:SS');
         console.log("退出ユーザー名:" + data);
         socket.broadcast.emit('receiveExitEvent', data, dtFormat);
     });
