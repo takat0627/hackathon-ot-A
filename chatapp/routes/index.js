d663434f1cf469c6e87fbfdfa2280a5a60cc69bf
LostGeneration1999 <masaya4426saganosen@gmail.com>
2020-09-05 21:13:36 +0900

    ページ遷移機能

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -1,19 +1,62 @@
 'use strict';
 
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
 const router = express.Router();
 
 // ログイン画面の表示
 router.get('/', function(request, response, next) {
-    response.render('index');
+    var session = request.session;
+    
+    response.render('index', { 'visitCount' :session.visitCount });
     // response.end();
 });
 
+// 個人タスク一覧画面
+// taskにPOSTした時の処理
+router.post('/user', function(request, response, next) {
+    const db = new sqlite3.Database('user.db');
+
+    // response.end();	    
+    // DBの処理
+    db.serialize(function () {
+        // テーブルがなければ作成
+        db.run(`CREATE TABLE IF NOT EXISTS user ( name TEXT )`);
+
+        // 名前を取得している
+        db.get(`SELECT name FROM user WHERE name = ${request.body.userName}`, function (err, row) {
+            if (!err) {
+                // express-sessionモジュールを用いて現在のユーザー情報を保持
+                request.session.username = request.body.userName ;
+            }
+        });
+
+        // もしなかったら追加する
+        if (request.session.username == null) {
+            console.log(request.session.username);
+            // prepare Statementでデータ挿入
+            let stmt = db.prepare(`INSERT INTO user VALUES (?)`);
+            stmt.run([request.body.userName]);
+            stmt.finalize();
+            // express-sessionモジュールを用いて現在のユーザー情報を保持
+            request.session.username = request.body.userName ;
+        }
+    });
+    db.close();
+    response.render('user', { userName: request.session.username });//入力値をuserNameに代入
+});
+
 // チャット画面の表示
-router.post('/room', function(request, response, next) {
-    console.log('ユーザ名：' + request.body.userName);
-    response.render('room', { userName: request.body.userName });//入力値をuserNameに代入
-    // response.end();
+router.get('/room', function (request, response, next){
+    // requestからユーザー情報を取得する
+    response.render('room', { userName: request.session.username });
+});
+
+// チャット画面の表示
+router.get('/task', function (request, response, next){
+    // requestからユーザー情報を取得する
+    response.render('task', { userName: request.session.username });
 });
 
 module.exports = router;
